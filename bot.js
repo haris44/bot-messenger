1019accac5d03f7ba33eaa66742ed31dd19627f3
Alexandre <lxndr.bertrand@gmail.com>
2018-03-22 12:39:30 +0100

    remove access env

--- bot.js
+++ bot.js
@@ -25,44 +25,10 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 
-function setGetStartedButton(res) {
-    const messageData = {
-        'get_started': {
-            'payload': 'get_started'
-        }
-    }
-
-    // Start the request
-    request({
-            url: 'https://graph.facebook.com/v2.6/me/messenger_profile?access_token=' + process.env.PAGE_ACCESS_TOKEN,
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            form: messageData
-        },
-        function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-                // Print out the response body
-                res.send('Bienvenue sur la page Thoune !')
-
-            } else {
-                // TODO: Handle errors
-                res.send(body)
-            }
-        })
-}
-
-app.get('/setup', function(req, res) {
-    setGetStartedButton(res)
-})
 
 // Webhook validation.
 app.get('/webhook', function(req, res) {
-    if (req.query['hub.mode'] === 'subscribe' &&
-        req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
-        res.status(200).send(req.query['hub.challenge'])
-    } else {
-        res.sendStatus(403)
-    }
+    res.status(200).send(req.query['hub.challenge'])
 })
 
 // Message processing
@@ -72,26 +38,17 @@ app.post('/webhook', function(req, res) {
     // Make sure this is a page subscription
     if (data.object === 'page') {
 
-        // Iterate over each entry - there may be multiple if batched
         data.entry.forEach(function(entry) {
-
-            // Iterate over each messaging event
             entry.messaging.forEach(function(event) {
                 if (event.message) {
                     receivedMessage(event)
-                } else if (event.postback.payload == 'get_started') {
-                    receivedPostback(event)
                 } else {
                     debug.log('Webhook received unknown event: ', event)
                 }
             })
         })
 
-        // Assume all went well.
-        //
-        // You must send back a 200, within 20 seconds, to let us know
-        // you've successfully  the callback. Otherwise, the request
-        // will time out and we will keep trying to resend.
+
         res.sendStatus(200)
     }
 })
@@ -120,59 +77,6 @@ function receivedMessage(event) {
     }
 }
 
-function receivedPostback(event) {
-
-    // When a postback is called, we'll send a message back to the sender to
-    // let them know it was successful
-    sendFirst(event, event.sender.id)
-}
-
-function sendFirst(event, userid) {
-    getProfile(userid)
-    const senderID = event.sender.id
-    const messageText = event.postback.title
-    const timestamp = Math.trunc(event.timestamp / 1000)
-    const messageId = event.postback.title
-
-
-    const messageData = {
-        'messageid': messageId,
-        'content': messageText,
-        'timestamp': timestamp.toString(),
-        'userid': senderID
-    }
-    const user = {
-        'userid': userid,
-        'nom': userLName,
-        'prenom': userFName
-    }
-
-    establishConnection.then((connectionEstablished) => {
-        publisher(connectionEstablished, messageData, user)
-    })
-}
-
-
-function getProfile(id) {
-    const user = {}
-    const userPublicInfo = 'https://graph.facebook.com/v2.6/' + id + '?fields=first_name,last_name&access_token=' + process.env.PAGE_ACCESS_TOKEN
-    request({
-        method: 'GET',
-        url: userPublicInfo,
-        json: true,
-        body: user
-    }, function(error, response, body) {
-        if (!error && response.statusCode === 200) {
-            debug.log('Get user works ' + body.first_name)
-            userFName = body.first_name
-            userLName = body.last_name
-        }
-    })
-}
-
-//////////////////////////
-// Sending helpers
-//////////////////////////
 
 // Set Express to listen out for HTTP requests
 const server = app.listen(process.env.PORT, function() {
--- rabbit/receiver.js
+++ rabbit/receiver.js
@@ -3,22 +3,22 @@ const debug = require('debug')
 const sendTextMessage = require('./sendTextMessage')
 
 module.exports = (conn) => {
-  conn.createChannel((err, ch) => {
-    const type = process.env.RABBIT_TYPE
-    const ex = process.env.RABBIT_EXCHANGE
-    const severity = process.env.RABBIT_BINDING_FACEBOOK
-    const queue_name = process.env.RABBIT_QUEUE_FACEBOOK
+    conn.createChannel((err, ch) => {
+        const type = process.env.RABBIT_TYPE
+        const ex = process.env.RABBIT_EXCHANGE
+        const severity = process.env.RABBIT_BINDING_FACEBOOK
+        const queue_name = process.env.RABBIT_QUEUE_FACEBOOK
 
-    ch.assertExchange(ex, type, { durable: true })
+        ch.assertExchange(ex, type, { durable: true })
 
-    ch.assertQueue(queue_name, { durable: false }, function (err, ok) {
+        ch.assertQueue(queue_name, { durable: false }, function(err, ok) {
 
-      ch.bindQueue(queue_name, ex, severity)
+            ch.bindQueue(queue_name, ex, severity)
 
-      ch.consume(queue_name, function (msg) {
-        let message = JSON.parse(msg.content.toString())
-        sendTextMessage(message.userid, message.content)
-      }, { noAck: true })
+            ch.consume(queue_name, function(msg) {
+                let message = JSON.parse(msg.content.toString())
+                sendTextMessage(message.userid, message.content, message.accessToken)
+            }, { noAck: true })
+        })
     })
-  })
 }
